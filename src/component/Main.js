11de875411875b2ef2da76b5c73d3b684d53e268
av84002499 <109796002+av84002499@users.noreply.github.com>
2023-06-26 22:43:40 +0530

    code

---
+++ src/component/Main.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Main.css';
+
+export default function Main() {
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <img className="logo-upper" src="assets/Z_svg.svg" alt="Z-logo image" />
+        <div className="zip-logo-container">
+          <div className="zip-text-wrapper">
+            <h1 className="zip-txt">ZIP</h1>
+            <p className="messenger-txt">Messenger</p>
+          </div>
+          <div className="zip-logo-wrapper">
+            <img className="logo1" src="assets/V_svg.svg" alt="V-logo image" />
+            <img className="logo2" src="assets/S_svg.svg" alt="S-logo image" />
+            <img className="logo3" src="assets/G_svg.svg" alt="G-logo image" />
+          </div>
+        </div>
+      </div>
+
+      <div className="sms-container">
+        <div className="sms-wrapper">
+          <div className="message-header">
+            <h1 className="main-header-txt">Message</h1>
+            <p className="side-txt">Enter message below</p>
+          </div>
+          <textarea className="message-txt" cols="30" rows="30"></textarea>
+        </div>
+      </div>
+
+      <div className="btn-container">
+        <button className="btn-send"><p>SEND</p></button>
+        <button className="btn-cancel"><p>CANCEL</p></button>
+      </div>
+    </div>
+  )
+}
---
+++ src/component/SectionMain.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./SectionMain.css";
+
+export default function SectionMain() {
+  return (
+    <div className='section'>
+    <div className='section-column'>
+      <h2 className='section-heading'>Section title 1</h2>
+      <label for="html" className='section-label'>Label Text</label>
+      <button className='section-button'>Button</button>
+    </div>
+    </div>
+  )
+}
